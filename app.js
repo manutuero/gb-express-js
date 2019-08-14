3b7e732f583f671ad0dffb2502c598795abe3388
Gabriel Bori <gabrielbori@MacBook-Pro-de-Gabo-Wolox.local>
2019-08-14 12:35:37 -0300

    Created proyect and excecuted initial commands

---
+++ app.js
@@ -0,0 +1,39 @@
+const { expressMiddleware, expressRequestIdMiddleware } = require('express-wolox-logger'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  config = require('./config'),
+  routes = require('./app/routes'),
+  errors = require('./app/middlewares/errors'),
+  DEFAULT_BODY_SIZE_LIMIT = 1024 * 1024 * 10,
+  DEFAULT_PARAMETER_LIMIT = 10000;
+
+const bodyParserJsonConfig = () => ({
+  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
+  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
+});
+
+const bodyParserUrlencodedConfig = () => ({
+  extended: true,
+  parameterLimit: config.common.api.parameterLimit || DEFAULT_PARAMETER_LIMIT,
+  limit: config.common.api.bodySizeLimit || DEFAULT_BODY_SIZE_LIMIT
+});
+
+const app = express();
+
+app.use('/docs', express.static(path.join(__dirname, 'docs')));
+
+// Client must send "Content-Type: application/json" header
+app.use(bodyParser.json(bodyParserJsonConfig()));
+app.use(bodyParser.urlencoded(bodyParserUrlencodedConfig()));
+app.use(expressRequestIdMiddleware);
+
+if (!config.isTesting) {
+  app.use(expressMiddleware);
+}
+
+routes.init(app);
+
+app.use(errors.handle);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app'),
+  migrationsManager = require('./migrations'),
+  config = require('./config'),
+  logger = require('./app/logger');
+
+const port = config.common.api.port || 8080;
+
+Promise.resolve()
+ // .then(() => migrationsManager.check())
+  .then(() => {
+    app.listen(port);
+
+    logger.info(`Listening on port: ${port}`);
+  })
+  .catch(logger.error);
