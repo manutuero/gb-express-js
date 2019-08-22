b673a6bfbcd4b3becd47d5501a2f3a008ba520e9
gaboxxz <gaboxxz@gmail.com>
2019-08-22 11:53:51 -0300

    working on albums requests

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,14 +1,19 @@
 const request = require('request');
 const logger = require('../logger');
 
-exports.getAlbums=(_, res1)=> {
-  request('https://jsonplaceholder.typicode.com/albums', {json: true}, (err, res, body) =>{
-    if (res.statusCode===404) {
+exports.getAlbums = (_, res1) => {
+  request('https://jsonplaceholder.typicode.com/albums', { json: true }, (err, res, body) => {
+    if (res.statusCode === 404) {
       return res1.status(404).send(body.explanation);
     }
     logger.info(body.info);
     logger.info(body.explanation);
-    res1.status(res.statusCode).send(body);
+    return res1.status(res.statusCode).send(body);
   });
 };
+
+exports.getIdAlbumPhotos = (req,res,next) => {
+
+return res.send();
+} ;
 
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,11 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
-const { getAlbums } = require('./controllers/albums');
+const { getAlbums,getIdAlbumPhotos } = require('./controllers/albums');
+
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.get('/albums',getAlbums);
+  app.get('/albums', getAlbums);
+  app.get('/albums/:id/photos',getIdAlbumPhotos);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
