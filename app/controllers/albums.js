ec00948da184f8ba6c507f9c78dcac349b683011
gaboxxz <gaboxxz@gmail.com>
2019-08-22 13:51:30 -0300

    Add function to make request and added get photos from album by id

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,19 +1,22 @@
 const request = require('request');
 const logger = require('../logger');
+const url = 'https://jsonplaceholder.typicode.com/'
 
+const makeRequest = (route,res1) => {
+    request(url+route, { json: true }, (err, res, body) => {
+        if (res.statusCode === 404) {
+          return res1.status(404).send(body.explanation);
+        }
+        logger.info(body.info);
+        logger.info(body.explanation);
+        return res1.status(res.statusCode).send(body);
+      });
+}
 exports.getAlbums = (_, res1) => {
-  request('https://jsonplaceholder.typicode.com/albums', { json: true }, (err, res, body) => {
-    if (res.statusCode === 404) {
-      return res1.status(404).send(body.explanation);
-    }
-    logger.info(body.info);
-    logger.info(body.explanation);
-    return res1.status(res.statusCode).send(body);
-  });
+    return makeRequest('albums',res1);
 };
 
 exports.getIdAlbumPhotos = (req,res,next) => {
-
-return res.send();
+return makeRequest('albums/'+req.query.id+'/photos',res);
 } ;
 
