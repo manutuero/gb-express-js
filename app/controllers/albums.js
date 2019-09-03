66602599f1e3101d1ac20585abe1bcdc171f4340
gaboxxz <gaboxxz@gmail.com>
2019-09-03 15:09:00 -0300

    Added logs and next to handle errors

--- app/controllers/albums.js
+++ app/controllers/albums.js
@@ -1,8 +1,16 @@
 const albums = require('../services/albums');
-
-exports.getAlbums = (_, res) => {
-  albums.getAlbums().then(json => res.status(200).send(json));
-};
+const logger = require('../../app/logger');
+exports.getAlbums = (_, res, next) =>
+  albums
+    .getAlbums()
+    .then(json => {
+      logger.info(JSON.stringify(json));
+      return res.status(200).send(json);
+    })
+    .catch(err => {
+      logger.error(err.message);
+      return next();
+    });
 exports.getPhotosByAlbumId = (req, res) => {
   albums.getIdAlbumPhotos(req.params.id).then(response => {
     if (parseInt(response.length)) {
