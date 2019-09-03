d8ede9f5171156272d08443569f47765272bcb64
gaboxxz <gaboxxz@gmail.com>
2019-09-03 14:50:45 -0300

    moved url to env

--- app/services/albums.js
+++ app/services/albums.js
@@ -1,12 +1,11 @@
 const fetch = require('node-fetch');
-const url = 'https://jsonplaceholder.typicode.com/';
 const logger = require('../../app/logger');
-exports.getAlbums = () => fetch(`${url}albums`).then(response => response.json());
+exports.getAlbums = () => fetch(`${process.env.ALBUMS_URL}albums`).then(response => response.json());
 
 const filerResponseById = (list, id) => list.filter(album => parseInt(album.albumId) === parseInt(id));
 
 exports.getIdAlbumPhotos = id =>
-  fetch(`${url}photos`)
+  fetch(`${process.env.ALBUMS_URL}photos`)
     .then(response => response.json())
     .then(jsonResponse => filerResponseById(jsonResponse, id))
     .catch(logger.error);
