7ca797124df17631c8a8ce61d73e8fbb511accbf
gaboxxz <gaboxxz@gmail.com>
2019-09-08 16:27:45 -0300

    Finished signUp tests

--- test/signUp.js
+++ test/signUp.js
@@ -1,31 +1,96 @@
 const app = require('../server');
+const db = require('../app/models');
+
 const supertest = require('supertest');
 
 const request = supertest(app);
+describe('Post /users', () => {
+  it('Create user', async done => {
+    const response = await request
+      .post('/users')
+      .set('Content-Type', 'application/json')
+      .set('Acccept', 'application/json')
+      .send({
+        firstName: 'TestName',
+        lastName: 'TestLastName',
+        password: '12345678Ab',
+        email: 'Test@wolox.com'
+      });
+    expect(response.status).toBe(201);
+    expect(response.body).not.toBe({});
+    expect(response.body.firstName).toBe('TestName');
+    expect(response.body.lastName).toBe('TestLastName');
+    expect(response.body.email).toBe('Test@wolox.com');
+    // expect(response.body.id).toBe();
+    done();
+  });
 
-it('Create user', async done => {
-  const response = await request
-    .post('/users')
-    .set('Content-Type', 'application/json')
-    .set('Acccept', 'application/json')
-    .send({
-      firstName: 'gabito',
-      lastName: 'bori',
-      password: 'dgkfkfkfkfk',
-      email: 'dollkjhgfdo@wolox.com'
+  it('Fails to create user, missing field', async done => {
+    const response = await request
+      .post('/users')
+      .set('Content-Type', 'application/json')
+      .set('Acccept', 'application/json')
+      .send({
+        firstName: 'TestName',
+        password: 'TestPassword',
+        email: 'Test@wolox.com'
+      });
+    expect(response.status).toBe(422);
+    expect(response.body.errors[0]).toMatchObject({
+      msg: 'Invalid value',
+      param: 'lastName',
+      location: 'body'
     });
-  // expect(response.status).toBe(200);
-  expect(response.body).not.toBe({});
-  expect(response.body.firstName).toBe('gabito');
-  expect(response.body.lastName).toBe('bori');
-  expect(response.body.email).toBe('dollkjhgfdo@wolox.com');
-  // expect(response.body.id).toBe();
+    done();
+  });
 
-  done();
+  it('Fails to create user, short pasasword', async done => {
+    const response = await request
+      .post('/users')
+      .set('Content-Type', 'application/json')
+      .set('Acccept', 'application/json')
+      .send({
+        firstName: 'TestName',
+        lastName: 'TestLastName',
+        password: '1234567',
+        email: 'Test@wolox.com'
+      });
+    expect(response.status).toBe(422);
+    expect(response.body.errors[0]).toMatchObject({
+      param: 'password',
+      msg: 'Password must be 8 characters long',
+      location: 'body'
+    });
+    done();
+  });
+
+  it('Fails to create user, user already exists', async done => {
+    await db.user.create({
+      firstName: 'TestName',
+      lastName: 'TestLastName',
+      password: '1234567',
+      email: 'Test@wolox.com'
+    });
+    const response = await request
+      .post('/users')
+      .set('Content-Type', 'application/json')
+      .set('Acccept', 'application/json')
+      .send({
+        firstName: 'TestName',
+        lastName: 'TestLastName',
+        password: '12345678',
+        email: 'Test@wolox.com'
+      });
+    expect(response.status).toBe(422);
+    expect(response.body.errors[0]).toMatchObject({
+      value: 'Test@wolox.com',
+      msg: {
+        message: 'User already exists.',
+        internalCode: 'database_error'
+      },
+      param: 'email',
+      location: 'body'
+    });
+    done();
+  });
 });
-
-// it('gets the albums wrong', async done => {
-//   const response = await request.get('/albums');
-
-//   done();
-// });
