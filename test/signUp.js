9640db084e8c23731597436364fc37c0f637210c
gaboxxz <gaboxxz@gmail.com>
2019-09-16 15:10:43 -0300

    re-doing tests with new changes from signUp

--- test/signUp.js
+++ test/signUp.js
@@ -1,11 +1,12 @@
 const app = require('../server');
 const db = require('../app/models');
-
 const supertest = require('supertest');
+const errors = require('../app/errors');
+const { paramsValidationsErrors } = require('../app/constants/errorsMessages');
 
 const validUser = {
-  firstName: 'TestName',
-  lastName: 'TestLastName',
+  first_name: 'TestName',
+  last_name: 'TestLastName',
   password: '12345678Ab',
   email: 'Test@wolox.com'
 };
@@ -20,26 +21,24 @@ describe('Post /users', () => {
       .send(validUser);
     expect(response.status).toBe(201);
     expect(response.body).not.toBe({});
-    expect(response.body.firstName).toBe(validUser.firstName);
-    expect(response.body.lastName).toBe(validUser.lastName);
-    expect(response.body.email).toBe(validUser.email);
+    expect(response.body.user.first_name).toBe(validUser.first_name);
+    expect(response.body.user.last_name).toBe(validUser.last_name);
+    expect(response.body.user.email).toBe(validUser.email);
     done();
   });
 
   it('Fails to create user, missing field', async done => {
     const { ...invalidUser } = validUser;
-    delete invalidUser.lastName;
+    delete invalidUser.last_name;
     const response = await request
       .post('/users')
       .set('Content-Type', 'application/json')
       .set('Acccept', 'application/json')
       .send(invalidUser);
-    expect(response.status).toBe(422);
-    expect(response.body.errors[0]).toMatchObject({
-      msg: 'Invalid value',
-      param: 'lastName',
-      location: 'body'
-    });
+    expect(response.status).toBe(400);
+    expect(response.body.message.length).toBe(1);
+    expect(response.body.message[0]).toBe('Invalid value');
+    expect(response.body.internal_code).toBe(errors.VALIDATION_ERROR);
     done();
   });
 
@@ -49,32 +48,21 @@ describe('Post /users', () => {
       .set('Content-Type', 'application/json')
       .set('Acccept', 'application/json')
       .send({ ...validUser, password: '1234567' });
-    expect(response.status).toBe(422);
-    expect(response.body.errors[0]).toMatchObject({
-      param: 'password',
-      msg: 'Password must be 8 characters long',
-      location: 'body'
-    });
+    expect(response.status).toBe(400);
+    expect(response.body.message.length).toBe(1);
+    expect(response.body.message[0]).toBe(paramsValidationsErrors.passwordLengthError);
+    expect(response.body.internal_code).toBe(errors.VALIDATION_ERROR);
     done();
   });
 
-  it('Fails to create user, user already exists', async done => {
+  it.only('Fails to create user, user already exists', async done => {
     await db.user.create(validUser);
     const response = await request
       .post('/users')
       .set('Content-Type', 'application/json')
       .set('Acccept', 'application/json')
       .send(validUser);
-    expect(response.status).toBe(422);
-    expect(response.body.errors[0]).toMatchObject({
-      value: 'Test@wolox.com',
-      msg: {
-        message: 'User already exists.',
-        internalCode: 'database_error'
-      },
-      param: 'email',
-      location: 'body'
-    });
+    expect(response.status).toBe(400);
     done();
   });
 });
